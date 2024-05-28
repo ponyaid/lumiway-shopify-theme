58676ebdbea1e130f4911c29a88b5bc381fea90d
ponyaid <moskvichovoleg@gmail.com>
2024-05-28 15:45:15 -0400

    main collection

--- assets/sliders.js
+++ assets/sliders.js
@@ -35,33 +35,4 @@ $(document).ready(() => {
       },
     ],
   });
-
-  $(".recommend-products__slider").slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: false,
-    infinite: true,
-    responsive: [
-      {
-        breakpoint: 1201,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 993,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
 });
